070fbdaeae0d3e4e848f948e7306f216611f3f55
Simon Gourdeau <simon.gourdeau@bell.ca>
2020-10-05 11:57:15 -0400

    Indent still bad

--- src/main/java/com/lesfurets/maven/partial/utils/DependencyUtils.java
+++ src/main/java/com/lesfurets/maven/partial/utils/DependencyUtils.java
@@ -18,34 +18,34 @@ public class DependencyUtils {
     public static Set<MavenProject> getAllDependencies(List<MavenProject> projects,
                                                        MavenProject project) {
         Stream<MavenProject> projectDeps = project.getDependencies().stream()
-            .map(d -> convert(projects, d))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .flatMap(p -> getAllDependencies(projects,
-                p).stream());
+                        .map(d -> convert(projects, d))
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .flatMap(p -> getAllDependencies(projects,
+                                        p).stream());
         Stream<MavenProject> pluginDeps = project.getBuildPlugins().stream()
-            .map(pl -> convert(projects, pl))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .flatMap(p -> getAllDependencies(projects,
-                p).stream());
+                        .map(pl -> convert(projects, pl))
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .flatMap(p -> getAllDependencies(projects,
+                                        p).stream());
         Stream<MavenProject>
-            dependencyMgmtDeps =
-            Optional.ofNullable(project.getDependencyManagement())
-                .map(dm -> dm.getDependencies().stream()
-                    .map(pl -> convert(projects,
-                        pl))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .flatMap(p -> getAllDependencies(
-                        projects,
-                        p).stream()))
-                .orElse(Stream.empty());
+                        dependencyMgmtDeps =
+                        Optional.ofNullable(project.getDependencyManagement())
+                                        .map(dm -> dm.getDependencies().stream()
+                                                        .map(pl -> convert(projects,
+                                                                        pl))
+                                                        .filter(Optional::isPresent)
+                                                        .map(Optional::get)
+                                                        .flatMap(p -> getAllDependencies(
+                                                                        projects,
+                                                                        p).stream()))
+                                        .orElse(Stream.empty());
         Set<MavenProject>
-            dependencies =
-            Stream.of(projectDeps, pluginDeps, dependencyMgmtDeps)
-                .flatMap(Function.identity())
-                .collect(Collectors.toSet());
+                        dependencies =
+                        Stream.of(projectDeps, pluginDeps, dependencyMgmtDeps)
+                                        .flatMap(Function.identity())
+                                        .collect(Collectors.toSet());
         dependencies.add(project);
         return dependencies;
     }
@@ -53,33 +53,33 @@ public class DependencyUtils {
     public static void collectDependents(List<MavenProject> projects, MavenProject project,
                                          Set<MavenProject> dependents) {
         projects.stream()
-            .filter(p -> project.equals(p.getParent()) || isDependentOf(p, project))
-            .filter(p -> !dependents.contains(p))
-            .forEach(p -> {
-                dependents.add(p);
-                if (!project.equals(p.getParent())) {
-                    collectDependents(projects, p, dependents);
-                }
-            });
+                        .filter(p -> project.equals(p.getParent()) || isDependentOf(p, project))
+                        .filter(p -> !dependents.contains(p))
+                        .forEach(p -> {
+                            dependents.add(p);
+                            if (!project.equals(p.getParent())) {
+                                collectDependents(projects, p, dependents);
+                            }
+                        });
     }
 
     public static void collectDependenciesInSnapshot(List<MavenProject> projects, MavenProject project,
                                                      Set<MavenProject> dependents) {
         projects.stream()
-            .filter(p -> project.equals(p.getParent())
-                || (isDependentOf(project, p) && p.getVersion().contains(SNAPSHOT)))
-            .filter(p -> !dependents.contains(p))
-            .forEach(p -> {
-                dependents.add(p);
-                if (!project.equals(p.getParent())) {
-                    collectDependenciesInSnapshot(projects, p, dependents);
-                }
-            });
+                        .filter(p -> project.equals(p.getParent())
+                                        || (isDependentOf(project, p) && p.getVersion().contains(SNAPSHOT)))
+                        .filter(p -> !dependents.contains(p))
+                        .forEach(p -> {
+                            dependents.add(p);
+                            if (!project.equals(p.getParent())) {
+                                collectDependenciesInSnapshot(projects, p, dependents);
+                            }
+                        });
     }
 
     private static boolean isDependentOf(MavenProject possibleDependent, MavenProject project) {
         return possibleDependent.getDependencies().stream().anyMatch(d -> equals(project, d)) ||
-            possibleDependent.getBuildPlugins().stream().anyMatch(p -> equals(project, p));
+                        possibleDependent.getBuildPlugins().stream().anyMatch(p -> equals(project, p));
     }
 
     private static Optional<MavenProject> convert(List<MavenProject> projects,
@@ -93,14 +93,14 @@ public class DependencyUtils {
 
     private static boolean equals(MavenProject project, Dependency dependency) {
         return dependency.getArtifactId().equals(project.getArtifactId())
-            && dependency.getGroupId().equals(project.getGroupId())
-            && dependency.getVersion().equals(project.getVersion());
+                        && dependency.getGroupId().equals(project.getGroupId())
+                        && dependency.getVersion().equals(project.getVersion());
     }
 
     private static boolean equals(MavenProject project, Plugin plugin) {
         return plugin.getArtifactId().equals(project.getArtifactId())
-            && plugin.getGroupId().equals(project.getGroupId())
-            && plugin.getVersion().equals(project.getVersion());
+                        && plugin.getGroupId().equals(project.getGroupId())
+                        && plugin.getVersion().equals(project.getVersion());
     }
 
 }
